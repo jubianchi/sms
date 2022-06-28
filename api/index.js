3337c8d450fef7a836a7c0bbbd2d92dee1af21df
jubianchi <contact@jubianchi.fr>
2022-06-28 21:10:34 +0200

    feat: Hook by user ID

--- api/index.js
+++ api/index.js
@@ -4,55 +4,39 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.all('/api/*', async (req, res) => {
+app.all('/api/:user', async (req, res) => {
   const { WebClient, LogLevel } = await import("@slack/web-api");
 
   const client = new WebClient(process.env.SLACK_TOKEN, {
-    logLevel: LogLevel.DEBUG
+    logLevel: LogLevel.INFO
   });
 
   await client.chat.postMessage({
-      channel: "U03200689",
+      channel: req.params.user,
       username: "SMS",
       text: `*From:* +${req.query.msisdn}\n*Text:*\n${req.query.text}`,
       icon_emoji: ":envelope:",
       "blocks": [
         {
           "type": "section",
+          "fields": [
+            {
+              "type": "mrkdwn",
+              "text": `*From:* +${req.query.msisdn}`
+            },
+            {
+              "type": "mrkdwn",
+              "text": `*To:* +${req.query.to}`
+            },
+            {
+              "type": "mrkdwn",
+              "text": `*Date:* ${req.query['message-timestamp']}`
+            },
+          ],
           "text": {
             "text": req.query.text,
             "type": "mrkdwn"
           },
-          "fields": [
-            {
-              "type": "mrkdwn",
-              "text": "*From*"
-            },
-            {
-              "type": "mrkdwn",
-              "text": "*To*"
-            },
-            {
-              "type": "plain_text",
-              "text": `+${req.query.msisdn}`
-            },
-            {
-              "type": "plain_text",
-              "text": `+${req.query.to}`
-            },
-            {
-              "type": "mrkdwn",
-              "text": "*Date*"
-            },
-            {
-              "type": "mrkdwn",
-              "text": " "
-            },
-            {
-              "type": "plain_text",
-              "text": req.query['message-timestamp']
-            },
-          ]
         }
       ]
   });
