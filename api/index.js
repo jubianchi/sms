ce33ea3265681f81f7239e56f1d20d7f1ecb0184
jubianchi <contact@jubianchi.fr>
2022-06-28 17:59:40 +0200

    add slack hook

--- api/index.js
+++ api/index.js
@@ -1,11 +1,24 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.all('/api/*', (req, res) => {
-  console.log(req.body);
+app.all('/api/*', async (req, res) => {
+  console.log(req.body, req.query);
+
+  await fetch('https://hooks.slack.com/services/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', {
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      channel: "@jubianchi",
+      username: "SMS",
+      text: `From: +${req.msisdn}\nText:\n${req.query.text}`,
+      icon_emoji: ":envelope:",
+    })
+  })
+
   res.status(200).end();
 });
 
