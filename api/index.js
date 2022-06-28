b86a8a115f3ebb60f802f5dad42ca8a27120add5
jubianchi <contact@jubianchi.fr>
2022-06-28 18:02:21 +0200

    add slack hook

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
+const fetch = import('node-fetch');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+///api/sms?msisdn=33626721260&to=33644638451&messageId=2B000000072C6C61&text=tg+pf&type=text&keyword=TG&api-key=f767567a&message-timestamp=2022-06-28+15%3A35%3A29
 app.all('/api/*', async (req, res) => {
   console.log(req.body, req.query);
 
