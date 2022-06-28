13dfbdff486b7e7884595f99a55a82a2934da65f
jubianchi <contact@jubianchi.fr>
2022-06-28 18:34:51 +0200

    Add slack hook

--- api/index.js
+++ api/index.js
@@ -1,24 +1,26 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
-const fetch = import('node-fetch');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 ///api/sms?msisdn=33626721260&to=33644638451&messageId=2B000000072C6C61&text=tg+pf&type=text&keyword=TG&api-key=f767567a&message-timestamp=2022-06-28+15%3A35%3A29
 app.all('/api/*', async (req, res) => {
+  const fetch = (await import('node-fetch')).default;
+  const { WebClient, LogLevel } = await import("@slack/web-api");
+
+  const client = new WebClient(process.env.SLACK_TOKEN, {
+    logLevel: LogLevel.DEBUG
+  });
+
   console.log(req.body, req.query);
 
-  await fetch('https://hooks.slack.com/services/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', {
-    method: 'post',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({
-      channel: "@jubianchi",
+  console.log(await client.chat.postMessage({
+      channel: "U03200689",
       username: "SMS",
-      text: `From: +${req.msisdn}\nText:\n${req.query.text}`,
+      text: `*From:* +${req.query.msisdn}\n*Text:*\n${req.query.text}`,
       icon_emoji: ":envelope:",
-    })
-  })
+  }));
 
   res.status(200).end();
 });
