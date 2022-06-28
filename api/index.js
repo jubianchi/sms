8bb2bf4def2c47041a0c90170e36a59ace5d2bf4
jubianchi <contact@jubianchi.fr>
2022-06-28 18:48:42 +0200

    better hook

--- api/index.js
+++ api/index.js
@@ -4,23 +4,58 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-///api/sms?msisdn=33626721260&to=33644638451&messageId=2B000000072C6C61&text=tg+pf&type=text&keyword=TG&api-key=f767567a&message-timestamp=2022-06-28+15%3A35%3A29
 app.all('/api/*', async (req, res) => {
-  const fetch = (await import('node-fetch')).default;
   const { WebClient, LogLevel } = await import("@slack/web-api");
 
   const client = new WebClient(process.env.SLACK_TOKEN, {
     logLevel: LogLevel.DEBUG
   });
 
-  console.log(req.body, req.query);
-
-  console.log(await client.chat.postMessage({
+  await client.chat.postMessage({
       channel: "U03200689",
       username: "SMS",
       text: `*From:* +${req.query.msisdn}\n*Text:*\n${req.query.text}`,
       icon_emoji: ":envelope:",
-  }));
+      "blocks": [
+        {
+          "type": "section",
+          "text": {
+            "text": req.query.text,
+            "type": "mrkdwn"
+          },
+          "fields": [
+            {
+              "type": "mrkdwn",
+              "text": "*From*"
+            },
+            {
+              "type": "mrkdwn",
+              "text": "*To*"
+            },
+            {
+              "type": "plain_text",
+              "text": `+${req.query.msisdn}`
+            },
+            {
+              "type": "plain_text",
+              "text": `+${req.query.to}`
+            },
+            {
+              "type": "mrkdwn",
+              "text": "*Date*"
+            },
+            {
+              "type": "mrkdwn",
+              "text": " "
+            },
+            {
+              "type": "plain_text",
+              "text": req.query['message-timestamp']
+            },
+          ]
+        }
+      ]
+  });
 
   res.status(200).end();
 });
